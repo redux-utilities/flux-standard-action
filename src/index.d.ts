5eb7a9c940acb4555fc7592e1e18e5fc72695da9
Homa Wong <homawong@gmail.com>
2016-11-18 14:34:09 -0800

    Add type guard, generic, and test

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-export interface FluxStandardAction {
+export interface FluxStandardAction<T, M> {
   /**
    * The `type` of an action identifies to the consumer the nature of the action that has occurred.
    * Two actions with the same `type` MUST be strictly equivalent (using `===`)
@@ -11,7 +11,7 @@ export interface FluxStandardAction {
    * By convention, if `error` is `true`, the `payload` SHOULD be an error object.
    * This is akin to rejecting a promise with an error object.
    */
-  payload?: any;
+  payload?: T;
   /**
    * The optional `error` property MAY be set to true if the action represents an error.
    * An action whose `error` is true is analogous to a rejected Promise.
@@ -23,20 +23,24 @@ export interface FluxStandardAction {
    * The optional `meta` property MAY be any type of value.
    * It is intended for any extra information that is not part of the payload.
    */
-  meta?: any
+  meta?: M
+}
+
+export interface ErrorFluxStandardAction<E extends Error, M> extends FluxStandardAction<E, M> {
+  error: true
 }
 
 /**
  * Alias to FluxStandardAction for shorthand
  */
-export type FSA = FluxStandardAction;
+export type FSA<T, M> = FluxStandardAction<T, M>;
 
 /**
  * Returns `true` if `action` is FSA compliant.
  */
-export function isFSA(action: any): boolean;
+export function isFSA<P, M>(action: any): action is FluxStandardAction<P, M>;
 
 /**
  * Returns `true` if `action` is FSA compliant error.
  */
-export function isError(action: any): boolean;
+export function isError<E extends Error>(action: any): action is ErrorFluxStandardAction<E, any>;
---
+++ test/typings-test.ts
@@ -0,0 +1,67 @@
+import { FluxStandardAction, isError, isFSA } from '../src'
+
+interface CustomPayload {
+  a: number
+}
+
+interface CustomMetadata {
+  b: string
+}
+
+interface MyError extends Error {
+  someField: string
+}
+
+function createCustomAction(payload: CustomPayload): FluxStandardAction<CustomPayload, any> {
+  return {
+    type: 'custom',
+    payload
+  }
+}
+
+function isCustomAction(action: FluxStandardAction<any, any>): action is FluxStandardAction<CustomPayload, any> {
+  return action.type === 'custom'
+}
+
+function isCustomAction2(action: FluxStandardAction<any, any>): action is FluxStandardAction<CustomPayload, CustomMetadata> {
+  return action.type === 'custom2'
+}
+
+function isCustomAction3(action: any): action is FluxStandardAction<void, string> {
+  return action.type === 'custom3'
+}
+
+function reducer(state, action) {
+  if (isFSA<CustomPayload, void>(action)) {
+    let a: number = action.payload.a
+  }
+  else if (isFSA<CustomPayload, CustomMetadata>(action)) {
+    let a: number = action.payload.a
+    let b: string = action.meta.b
+  }
+  else if (isFSA<void, string>(action)) {
+    let meta: string = action.meta
+  }
+  else if (isError(action)) {
+    let iserr: true = action.error // iserr === true
+    let err: Error = action.payload
+  }
+  else if (isError<MyError>(action)) {
+    let err: MyError = action.payload
+  }
+}
+
+function reducer2(state, action) {
+  if (isCustomAction(action)) {
+    let a: number = action.payload.a
+  }
+  else if (isCustomAction2(action)) {
+    let a: number = action.payload.a
+    let b: string = action.meta.b
+  }
+  else if (isCustomAction3(action)) {
+    let meta: string = action.meta
+  }
+}
+
+let action = createCustomAction({ a: 123 })
