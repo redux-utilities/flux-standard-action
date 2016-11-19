8c64c1bee6339d7ed456d0672b83da4a17d4028f
Homa Wong <homawong@gmail.com>
2016-11-18 19:49:59 -0800

    Add semi-colon, move tsc to posttest

--- test/typings-test.ts
+++ test/typings-test.ts
@@ -1,4 +1,4 @@
-import { FluxStandardAction, isError, isFSA } from '../src'
+import { FluxStandardAction, isError, isFSA } from '../src';
 
 interface CustomPayload {
   a: number
@@ -16,53 +16,53 @@ function createCustomAction(payload: CustomPayload): FluxStandardAction<CustomPa
   return {
     type: 'custom',
     payload
-  }
+  };
 }
 
 function isCustomAction(action: FluxStandardAction<any, any>): action is FluxStandardAction<CustomPayload, any> {
-  return isFSA(action) && action.type === 'custom'
+  return isFSA(action) && action.type === 'custom';
 }
 
 function isCustomAction2(action: FluxStandardAction<any, any>): action is FluxStandardAction<CustomPayload, CustomMetadata> {
-  return isFSA(action) && action.type === 'custom2'
+  return isFSA(action) && action.type === 'custom2';
 }
 
 function isCustomAction3(action: any): action is FluxStandardAction<void, string> {
-  return isFSA(action) && action.type === 'custom3'
+  return isFSA(action) && action.type === 'custom3';
 }
 
 function reducer(state, action) {
   if (isFSA<CustomPayload, void>(action)) {
-    let a: number = action.payload.a
+    let a: number = action.payload.a;
   }
   else if (isFSA<CustomPayload, CustomMetadata>(action)) {
-    let a: number = action.payload.a
-    let b: string = action.meta.b
+    let a: number = action.payload.a;
+    let b: string = action.meta.b;
   }
   else if (isFSA<void, string>(action)) {
-    let meta: string = action.meta
+    let meta: string = action.meta;
   }
   else if (isError(action)) {
-    let iserr: true = action.error // iserr === true
-    let err: Error = action.payload
+    let iserr: true = action.error; // iserr === true
+    let err: Error = action.payload;
   }
   else if (isError<MyError, void>(action)) {
-    let err: MyError = action.payload
-    let someFieldValue: string = err.someField
+    let err: MyError = action.payload;
+    let someFieldValue: string = err.someField;
   }
 }
 
 function reducer2(state, action) {
   if (isCustomAction(action)) {
-    let a: number = action.payload.a
+    let a: number = action.payload.a;
   }
   else if (isCustomAction2(action)) {
-    let a: number = action.payload.a
-    let b: string = action.meta.b
+    let a: number = action.payload.a;
+    let b: string = action.meta.b;
   }
   else if (isCustomAction3(action)) {
-    let meta: string = action.meta
+    let meta: string = action.meta;
   }
 }
 
-let action = createCustomAction({ a: 123 })
+let action = createCustomAction({ a: 123 });
