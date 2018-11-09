ddbdf6cfdb82a558f22872ad82ebd730200b18d5
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-11-09 23:57:41 +0100

    fixup! Simplified FSA TypeScript test
    Fix and refactor TypeScript tests

--- test/typeFSA.test.ts
+++ test/typeFSA.test.ts
@@ -8,10 +8,15 @@ const assertNever = (x: never): never => {
   throw new Error(`Unexpected value: ${x}.`);
 };
 
+const assertTypeValue = (fsa: FSA_ACTION_TYPE_1) => {
+  expect(fsa.type).toBe(ACTION_TYPE_1);
+};
+
 describe('FluxStandardAction<Payload, Meta, Type>', () => {
   it('enables TypeScript action type enforcement', () => {
     const fsa_strict: FSA_ACTION_TYPE_1 = { type: ACTION_TYPE_1 };
-expect(fsaStrict.type).not.toBe(ACTION_TYPE_1)
+    assertTypeValue(fsa_strict);
+    if (fsa_strict.type !== ACTION_TYPE_1) {
       throw assertNever(fsa_strict.type);
     }
   });
