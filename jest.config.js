f47590578216db7f76825c5731525ca5d55a05ce
Jason Kurian <jasonk92@gmail.com>
2019-04-14 14:06:23 -0400

    chore: fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
   transform: {
     '^.+\\.js$': 'babel-jest',
     '^.+\\.tsx?$': 'ts-jest'
